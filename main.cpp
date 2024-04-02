e769c6e337e03806e5876e0a1f2fc9294f7cd21d
Arshdeep Sharma <118055889+CodeWithArshdeep@users.noreply.github.com>
2024-04-02 18:23:19 +0000

    add some more data types

--- main.cpp
+++ main.cpp
@@ -8,9 +8,12 @@ int main() {
   float f = 1.2;  
   double d = 1.23;
   bool b = true;
+  int c = 'c';                   //typecasting
   cout << a << " " << ch << " "<< f << " "<< d << " "<< b << " " << endl;
 
   int size = sizeof(a);
   cout << "Size of a is : " << size << endl;
+  cout << c << endl;           //tpyecasting
+}
   
-  }
+  
