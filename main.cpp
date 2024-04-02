2066f5b45af8d5784f34c059537248a4dd9a8a22
Arshdeep Sharma <118055889+CodeWithArshdeep@users.noreply.github.com>
2024-04-02 18:10:01 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "There are types of data variables" << endl;
+  int a = 5;
+  char ch = 'a';
+  float f = 1.2;  
+  double d = 1.23;
+  bool b = true;
+  cout << a << " " << ch << " "<< f << " "<< d << " "<< b << " " << endl;
+
+  int size = sizeof(a);
+  cout << "Size of a is : " << size << endl;
+  }
